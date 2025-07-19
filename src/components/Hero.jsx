14ced18e04e44ded607dc54928d5bdc03fc851ee
75781040+I0ANA24 <75781040+I0ANA24@users.noreply.github.com>
2025-07-19 12:01:45 +0300

    darkMode functionality

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <div>
+        <p className='text-white text-4xl'>I love matt</p>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
