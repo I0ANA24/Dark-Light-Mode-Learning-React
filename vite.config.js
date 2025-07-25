4d209e3010ee2fd56f4ee1ce30802421c5b0685f
75781040+I0ANA24 <75781040+I0ANA24@users.noreply.github.com>
2025-07-25 13:54:31 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Dark-Light-Mode-Learning-React-/', // 👈 adăugat pentru GitHub Pages
   plugins: [react(), tailwindcss()],
 })
